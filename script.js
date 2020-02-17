662c33b04c2c911d8473eb5376a45f775f74a9ff
Hamzah Bawah <bhamza123@gmail.com>
2020-02-17 00:17:02 +0000

    Added training models and face api

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let video = document.getElementById("vid");
 
+
+             
+
 let startVideo = ()=>{
     navigator.getUserMedia(
             {video:{}},
@@ -8,4 +11,17 @@ let startVideo = ()=>{
         )
 }
 
-startVideo();
+
+Promise.all([faceapi.nets.tinyFaceDetector.loadFromUri("models"),
+             faceapi.nets.faceLandmark68Net.loadFromUri("models"),
+             faceapi.nets.faceRecognitionNet.loadFromUri("models"),
+             faceapi.nets.faceExpressionNet.loadFromUri("models")
+            ]).then(startVideo())
+
+video.addEventListener("play", ()=>{
+    setInterval(async () => {
+       const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceExpressions();
+       console.log(detections)
+        
+    }, 1000);
+})
